1e6c6ac22bc2d3b0ee669bd3ff3d4f7e3d98d687
fatih.dogmus <fatih.dogmus@tubitak.gov.tr>
2022-04-22 15:19:23 +0300

    initial commit

---
+++ apps/ngrx-slice-type-inference-error/jest.config.ts
@@ -0,0 +1,21 @@
+module.exports = {
+  displayName: 'ngrx-slice-type-inference-error',
+  preset: '../../jest.preset.ts',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
+  coverageDirectory: '../../coverage/apps/ngrx-slice-type-inference-error',
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
